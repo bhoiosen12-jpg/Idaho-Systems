d7476810893f3881385142a7cf92d8da4482abf7
bhoiosen12-jpg <bhoiosen12@gmail.com>
2026-06-14 21:00:18 -0600

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -28,7 +28,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Idaho Systems",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 6, 
       },
     ],
   },
